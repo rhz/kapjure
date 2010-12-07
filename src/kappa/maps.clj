011243aa747a74c0b8782156b680d1369c7a58ee
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2010-12-06 21:13:40 -0300

    counted-sorted-sets (finger-trees) used to store matchings in matching map.
    maps.clj has been refactored.

--- src/kappa/maps.clj
+++ src/kappa/maps.clj
@@ -1,10 +1,11 @@
-(ns ^{:doc "Fns to create the activation map, inhibition map, matching map and lift map."
-      :author "Ricardo Honorato-Zimmer"}
-  kappa.maps
+(ns kappa.maps
+  {:doc "Fns to create the activation map, inhibition map, matching map and lift map."
+   :author "Ricardo Honorato-Zimmer"}
   (:require [kappa.language :as lang]
             [kappa.misc :as misc]
+            [clojure.set :as set]
             [clojure.contrib.condition :as c]
-            [clojure.set :as set]))
+            [clojure.data.finger-tree :as ft]))
 
 ;;; Activation and Inhibition map
 
@@ -39,12 +40,12 @@
       (lang/make-agent name states bindings))
     (handle :unmixable-agents nil)))
 
-(defn- create-mixed-expr
+(defn- make-minimal-mixed-expr
   "Returns the smallest expression mixing e1 and e2."
   [e1 e2]
   (let [[e1-e2 & left] (lang/pair-exprs e1 e2)]
-    (loop [agent-pairs e1-e2,
-           ids-map (zipmap (apply concat (map keys left)) (repeatedly #(misc/counter))),
+    (loop [agent-pairs e1-e2
+           ids-map (zipmap (apply concat (map keys left)) (repeatedly #(misc/counter)))
            result (into {} (apply concat
                                   (map (partial map (fn [[id a]] [(ids-map id) a])) left)))]
       (if (empty? agent-pairs)
@@ -54,40 +55,25 @@
 
         (let [[oae1 oae2] (first agent-pairs)]
           (if-let [mixed-agent (mix (val oae1) (val oae2) e1-e2)]
+            ;; mixable agents
             (let [id (misc/counter)]
               (recur (rest agent-pairs)
                      (into ids-map [{(key oae1) id} {(key oae2) id}])
                      (conj result {id mixed-agent})))
-          
+
+            ;; unmixable agents
             (let [id1 (misc/counter), new-oae1 {id1 (val oae1)},
                   id2 (misc/counter), new-oae2 {id2 (val oae2)}]
               (recur (rest agent-pairs)
                      (into ids-map [{(key oae1) id1} {(key oae2) id2}])
                      (into result [new-oae1 new-oae2])))))))))
 
-(defn codomain
-  "Returns a map from pairs [oae, site] in p to pairs [oae, site] in e that matches.
-  This function requires that the given expressions has accurate information about
-  their complexes in the metadata."
-  [p e]
-  (apply merge ;; don't replace this by into {}! remember that it returns a lot of empty maps
-         (let [e-complexes (map (partial lang/subexpr e) (-> e meta :complexes))]
-           (for [p-complex (map (partial lang/subexpr p) (-> p meta :complexes))
-                 
-                 matched-complex (keep (partial lang/match p-complex) e-complexes)
-                 
-                 ;; for each site in each agent of p-complex
-                 [pa-id pa :as p-oae] p-complex, pa-site (-> pa :states keys)
-                 
-                 :let [ea-id (matched-complex pa-id),  e-oae (find e ea-id)]]
-             {[p-oae pa-site] [e-oae pa-site]}))))
-
-(defn modified-site-in-mixed-expr-codomain [mss e1 e2]
+(defn find-modified-site-in-mixed-expr-codomain [mss e1 e2]
   (let [;; compute a minimal mixed expression between e1 and e2
-        S (lang/with-complexes (create-mixed-expr e1 e2)),
+        S (lang/with-complexes (make-minimal-mixed-expr e1 e2)),
         
         ;; compute the codomains of e1 and e2 in S
-        [e1-S e2-S] (map codomain [e1 e2] (repeat S)),
+        [e1-S e2-S] (map lang/domain2codomain [e1 e2] (repeat S)),
         
         ;; compute the intersection between the sites in the codomains of e1 and e2 in S
         cod (apply set/intersection (map (comp set vals) [e1-S e2-S])),
@@ -104,8 +90,8 @@
   "Tells whether r1 activates r2."
   [r1 r2]
   (boolean
-   (modified-site-in-mixed-expr-codomain (-> r1 :action meta :modified-sites) ;; mss
-                                         (:rhs r1) (:lhs r2))))
+   (find-modified-site-in-mixed-expr-codomain (-> r1 :action meta :modified-sites)
+                                              (:rhs r1) (:lhs r2))))
 
 (defn activation-map
   "Returns a map from each rule r in rules to the rules r activates."
@@ -120,8 +106,8 @@
   (if (identical? r1 r2)
     false
     (boolean
-     (modified-site-in-mixed-expr-codomain (-> r1 :action meta :modified-sites) ;; mss
-                                           (:lhs r1) (:lhs r2)))))
+     (find-modified-site-in-mixed-expr-codomain (-> r1 :action meta :modified-sites)
+                                                (:lhs r1) (:lhs r2)))))
 
 (defn inhibition-map
   "Returns a map from each rule r in rules to the rules r inhibits."
@@ -132,42 +118,51 @@
 
 
 ;;; Matching map and Lift map
+(defn- map-compare [m1 m2]
+  (compare (vec m1) (vec m2)))
 
 (defn matching-and-lift-map
   "Compute the matching map and lift map. For reference, see
   'Scalable Simulation of Cellular Signaling Networks', V. Danos, J. Feret,
   W. Fontana, and J. Krivine, 2007."
   [rule-set mixture]
-  (->> (for [r rule-set, cr (-> r :lhs meta :complexes)] ; for each pair [r, cr]
-         ;; cr and cm are seqs of ids... matchings is a seq of sets of ids
-         (let [cr-expr (with-meta (lang/subexpr (:lhs r) cr)
-                         {:complexes [cr]})
-               aux (keep (fn [cm] (codomain cr-expr (with-meta (lang/subexpr mixture cm)
-                                                      {:complexes [cm]})))
-                         (-> mixture meta :complexes))
-               cods (map (comp (partial map (fn [[[id a] s]] [id s])) vals) aux)
-               matchings (map (fn [cr->cm]
-                                (into {} (map (fn [[[[id1 a1] s1] [[id2 a2] s2]]]
-                                                [id1 id2]) cr->cm))) aux)]
-           ;;  matchings (map (comp set (partial map first)) cods)]
-           (vector
-            ;; for the matching map
-            {r {cr (set matchings)}}
-            ;; for the lift map
-            (for [cod cods, [a-id s] cod]
-              {a-id {s [{:rule r, :complex cr, :codomain cod}]}}))))
-       (apply map vector) ; put what belongs to the matching-map together in a vector
-       ;; and what belongs to the lift-map together in another vector
-       ;; note: map could be used for the next thing too in this way: (map #(%1 %2) [f1 f2])
-       ((juxt (comp (partial apply merge-with merge) ; if the same rule is found twice in the
-                    ;; matching map, merge the maps {cr matchings}, as cr won't appear twice
-                    first)
-              (comp (partial apply merge-with ; if the same agent is found twice in the lift map,
-                             ;; merge the maps associated with its sites using vector and then set
-                             ;; (as oppposed to the matching map, sites may appear twice)
-                             (partial merge-with concat))
-                    (partial apply concat) ;; but first make just one big list of all the maps
-                    second)))))
+  (->> (for [r rule-set
+             cr (-> r :lhs meta :complexes)
+             :let [cr-expr (lang/subexpr (:lhs r) cr)
+                   
+                   cr2cms (keep (fn cr2cms-gen-fn [cm]
+                                  (let [cm-expr (lang/subexpr mixture cm)]
+                                    (lang/complex-dom2cod mixture [cr-expr] [cm-expr])))
+                                (-> mixture meta :complexes))
+
+                   ;; take the ids and sites in the codomain
+                   cods (for [cr2cm cr2cms]
+                          (for [[[id _] s] (vals cr2cm)]
+                            [id s]))
+               
+                   ;; for each cr make a map from rule agent ids to mixture agent ids
+                   matchings (for [cr2cm cr2cms]
+                               (into {} (for [[[[ar-id _] _] [[am-id _] _]] cr2cm]
+                                          [ar-id am-id])))]]
+         
+         [;; this goes to the matching map
+          {r {cr (apply ft/counted-sorted-set-by map-compare matchings)}}
+          ;; and this to the lift map
+          (for [cod cods
+                [a-id s] cod]
+            {a-id {s [{:rule r, :complex cr, :codomain cod}]}})])
+    
+    (apply map vector) ; put what belongs to the matching-map together in a vector ...
+    ;; ... and what belongs to the lift-map together in another vector
+    ((partial map #(%1 %2)
+              [(partial apply merge-with merge) ;; if the same rule is found twice in the
+               ;; matching map, merge the maps {cr matchings}, as cr won't appear twice
+               (comp (partial apply merge-with ;; if the same agent is found twice in the
+                              ;; lift map, merge the maps associated with its sites using
+                              ;; vector and then set (in lift map, sites may appear twice)
+                              (partial merge-with (comp doall concat)))
+                     ;; but first make just one big list of all the maps
+                     (partial apply concat))]))))
 
 ;;; Observables map: like a matching map for observed expressions
 (defn obs-map
