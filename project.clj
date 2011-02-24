158ff410fa58c283ff4b507fd9943cd8706deeb1
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2011-02-24 17:28:52 -0300

    project.clj updated

--- project.clj
+++ project.clj
@@ -3,12 +3,9 @@
   :url "http://github.com/rhz/kapjure"
   :license {:name "GNU General Public License v3.0"
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :tasks [protobuf.tasks]
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]
                  [org.clojars.ohpauleez/fnparse "3.0.0alpha4"]
                  [clojure.data.finger-tree "0.0.1-SNAPSHOT"]
                  [incanter "1.2.3"]]
-  :aot [kappa.isomerization kappa.simple-models]
   :warn-on-reflection true)
-
