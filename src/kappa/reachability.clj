60a01128e0dc57bba455155e310110be5f9bd383
Ricardo Honorato-Zimmer <rikardo.horo@gmail.com>
2011-02-25 17:10:39 -0300

    several improvements in reachability.clj
    * detects more complexes/reactions
    * less and simpler code

--- src/kappa/reachability.clj
+++ src/kappa/reachability.clj
@@ -34,25 +34,14 @@
 (defn new-ids [expr]
   (reduce new-id expr (keys expr)))
 
-(defn- matching-map [rules cs]
-  (->> (for [r rules
+(defn update-matching-map [mm rules cs]
+  (->> (for [cm-expr (map lang/with-complexes cs)
+             r rules
              cr (-> r :lhs meta :complexes)
-             cm-expr cs
              :let [cr-expr (lang/subexpr (:lhs r) cr)
                    matching (lang/match-expr cr-expr cm-expr)]
              :when matching]
-         {r {cr #{(vary-meta (lang/with-complexes cm-expr) into {:matching matching})}}})
-       (apply merge-with (partial merge-with into))))
-
-(defn- update-matching-map [mm c+rs ram]
-  (->> (for [[cm-expr executed-rule] c+rs
-             activated-rule (ram executed-rule)
-             cr (-> activated-rule :lhs meta :complexes)
-             :let [cm-expr (lang/with-complexes cm-expr)
-                   cr-expr (lang/subexpr (:lhs activated-rule) cr)
-                   matching (lang/match-expr cr-expr cm-expr)]
-             :when matching]
-         {activated-rule {cr #{(vary-meta cm-expr into {:matching matching})}}})
+         {r {cr #{(vary-meta cm-expr into {:matching matching})}}})
        (apply merge-with (partial merge-with into) mm)))
 
 ;;; Complexes
@@ -63,41 +52,32 @@
       lang/with-complexes))
 
 (defn- produced-complexes [cs mm]
-  (for [[r m] mm
-        matching (map (partial zipmap (keys m)) (apply comb/cartesian-product (vals m)))
-        :when (= (count matching) (-> r :lhs meta :complexes count))
-        :let [reactants-expr (apply lang/mix-exprs (vals matching))
-              matching (zipmap (keys matching) (map (comp :matching meta) (vals matching)))
-              products-expr (get-products r matching reactants-expr)
-              product-cs (lang/complexes products-expr)
-              new-cs (remove #(some (partial lang/match-expr %) cs) product-cs)]
-        :when (seq new-cs)]
-    [(map new-ids new-cs) r]))
+  (->> (for [[r m] mm
+             matching (map (partial zipmap (keys m)) (apply comb/cartesian-product (vals m)))
+             :when (= (count matching) (-> r :lhs meta :complexes count))
+             :let [reactants-expr (apply lang/mix-exprs (vals matching))
+                   matching (zipmap (keys matching) (map (comp :matching meta) (vals matching)))
+                   products-expr (get-products r matching reactants-expr)
+                   product-cs (lang/complexes products-expr)
+                   new-cs (remove #(some (partial lang/match-expr %) cs) product-cs)]
+             :when (seq new-cs)]
+         (map new-ids new-cs))
+       (apply concat)))
 
-(defn- distinct-complexes [cs+rs]
-  (let [m (into {} (for [[cs r] cs+rs, c cs] [c r]))
-        distinct-cs (distinct-exprs (keys m) [])]
-    (for [c distinct-cs] [c (m c)])))
-
-(defn- gen-complexes
-  [cs mm ram]
-  (let [c+rs (distinct-complexes (produced-complexes cs mm))]
-    (if (seq c+rs)
-      (let [new-cs (map first c+rs)]
-        ;;(doseq [c new-cs] (println c))
-        (concat new-cs (lazy-seq
-                         (gen-complexes (concat cs new-cs)
-                                        (update-matching-map mm c+rs ram) ram))))
+(defn- gen-complexes [rules cs mm]
+  (let [new-cs (distinct-exprs (produced-complexes cs mm) [])]
+    (if (seq new-cs)
+      (concat new-cs (lazy-seq
+                       (gen-complexes rules (concat cs new-cs)
+                                      (update-matching-map mm rules new-cs))))
       nil)))
 
 (defn reachable-complexes
   "Returns a lazy seq of all the complexes reachable by the system."
   [rules expr]
-  (let [cs (lang/complexes expr)
-        distinct-cs (distinct-exprs cs nil)]
-    (concat distinct-cs (gen-complexes distinct-cs
-                                       (matching-map rules distinct-cs)
-                                       (maps/activation-map rules)))))
+  (let [distinct-cs (distinct-exprs (lang/complexes expr) [])]
+    (concat distinct-cs (gen-complexes rules distinct-cs
+                                       (update-matching-map {} rules distinct-cs)))))
 
 ;;; Reactions
 
@@ -106,10 +86,10 @@
        (lang/match-expr (:rhs r1) (:rhs r2))))
 
 ;; TODO unify this fn with produced-complexes
-(defn- possible-reactions [cs rs mm]
+(defn- possible-reactions [cs rxns mm]
   (for [[r m] mm
         matching (map (partial zipmap (keys m)) (apply comb/cartesian-product (vals m)))
-        ;; why cartesian-product?... so it'll take one expression complex for the first rule
+        ;; we use the cartesian-product so it'll take one expression complex for the first rule
         ;; complex, one expression complex for the second rule complex and so on
         :when (= (count matching) (-> r :lhs meta :complexes count))
         :let [reactants-expr (apply lang/mix-exprs (vals matching))
@@ -119,29 +99,24 @@
               ;; we need this for update-matching-map
               new-cs (remove #(some (partial lang/match-expr %) cs) product-cs)
               rule-instance (lang/make-rule (:name r) reactants-expr products-expr (:rate r))]
-        :when (not-any? (partial match-rule rule-instance) rs)]
-    [(map new-ids new-cs) r rule-instance]))
+        :when (not-any? (partial match-rule rule-instance) rxns)]
+    [(map new-ids new-cs) rule-instance]))
 
 ;; TODO unify this fn with gen-complexes
-(defn- gen-rule-instances
-  [cs rs mm ram]
-  (let [cs+r+rxns (possible-reactions cs rs mm)]
-    (if (seq cs+r+rxns)
-      (let [c+rs (distinct-complexes (map (partial take 2) cs+r+rxns))
-            rxns (map #(nth % 2) cs+r+rxns)]
-        ;;(doseq [rxn rxns] (println rxn))
-        (concat rxns (lazy-seq
-                       (gen-rule-instances (concat cs (map first c+rs))
-                                           (concat rs rxns)
-                                           (update-matching-map mm c+rs ram) ram))))
+(defn- gen-rule-instances [rules cs rxns mm]
+  (let [cs+rxns (possible-reactions cs rxns mm)]
+    (if (seq cs+rxns)
+      (let [new-cs (distinct-exprs (mapcat first cs+rxns) [])
+            new-rxns (map second cs+rxns)]
+        (concat new-rxns (lazy-seq
+                           (gen-rule-instances rules (concat cs new-cs) (concat rxns new-rxns)
+                                               (update-matching-map mm rules new-cs)))))
       nil)))
 
 (defn get-reactions
   "Returns a lazy seq of all the rule instances (i.e., reactions) reachable by the system."
   [rules expr]
-  (let [cs (lang/complexes expr)
-        distinct-cs (distinct-exprs cs nil)]
-    (gen-rule-instances distinct-cs nil
-                        (matching-map rules distinct-cs)
-                        (maps/activation-map rules))))
+  (let [distinct-cs (distinct-exprs (lang/complexes expr) [])]
+    (gen-rule-instances rules distinct-cs []
+                        (update-matching-map {} rules distinct-cs))))
 
